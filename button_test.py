e3f44605d07d9cf0f0217385119f198171070eb0
CHRISTOPH Kormesser <christoph.kormesser@ibm.com>
2018-11-15 14:43:23 +0100

    changed button_test.py for 2 buttons

--- button_test.py
+++ button_test.py
@@ -4,9 +4,13 @@ import time
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
-    input_state = GPIO.input(10)
-    if input_state == False:
-        print('Button Pressed')
-        time.sleep(0.3)
+
+   if (GPIO.input(10) == True):
+      print ("Pin 10 is true")
+      # do stuff based on pin 10 here
+   elif(GPIO.input(21) == True):
+      print ("Pin 21 is true")
+      # do stuff based on pin 21 here
