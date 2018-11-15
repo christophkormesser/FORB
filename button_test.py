3bf72d4726697ddf26680862f26644d3c9afad6c
CHRISTOPH Kormesser <christoph.kormesser@ibm.com>
2018-11-15 13:16:30 +0100

    corrected a line in button_test.py

--- button_test.py
+++ button_test.py
@@ -2,7 +2,7 @@ import RPi.GPIO as gpio
 
 gpio.setwarnings(False)
 gpio.setmode(gpio.BOARD)
-gpio.setup(10, gpio.in, pull_up_down=gpio.PUD_DOWN)
+gpio.setup(10, gpio.IN, pull_up_down=gpio.PUD_DOWN)
 
 while True:
     if gpio.input(10) == gpio.HIGH:
