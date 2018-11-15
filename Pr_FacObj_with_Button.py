012674e5b0bc6c7658ddab39d322c7f5dd5b5d6a
Christoph Kormesser <christoph.kormesser@gmail.com>
2018-11-15 16:10:57 +0100

    Added Pr_FacObj_with_Button.py (Corrupted)

---
+++ Pr_FacObj_with_Button.py
@@ -0,0 +1,130 @@
+#!/usr/bin/python3
+#-*- coding: utf-8 -*-
+
+import requests, time, os
+from pathlib import Path
+import RPi.GPIO as GPIO
+from time import sleep
+import webbrowser
+
+
+import cv2
+import numpy as np
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+
+while True:
+    try:
+        r = requests.get("http://localhost:1880")
+        status = r.status_code
+
+        if status == 200:
+            webbrowser.open("http://localhost:1880", new=1, autoraise=False)
+
+    except requests.ConnectionError:
+        continue
+
+    break
+
+sleep(10)
+
+os.system("chromium-browser file:///home/pi/Desktop/Bot/venv/bin/cutie.html")
+time.sleep(5)
+os.system("xdotool key F11")
+
+
+os.system("omxplayer /home/pi/Downloads/beep.wav")
+
+while True:
+    if (GPIO.input(10) == False):
+        print ("Pin 10 is true")
+        cap = cv2.VideoCapture(0)
+        ret, frame = cap.read()
+        cv2.imwrite('/home/pi/Desktop/Bot/venv/Picture/image.png', frame)
+        cap.release()
+        cv2.destroyAllWindows()
+
+        imagePath = Path("/home/pi/Desktop/Bot/venv/Picture/image.png")
+
+        #setting variables
+        url= "http://localhost:1880/ORBface"
+
+        file ={'image':open(str(imagePath),'rb')}
+
+        #sending a request to node red
+        r = requests.post(url, files=file)
+        print(r.text)
+
+        
+        time.sleep(0.3)
+        
+  #"""elif(GPIO.input(21) == False):""""
+   #    time.sleep(0.3)
+    #   print ("Pin 21 is true")
+      # do stuff based on pin 21 here"""
+
+
+    #UI = input("f/o: ")
+
+   # if UI == "f":
+        #cap = cv2.VideoCapture(0)
+
+        # Capture frame-by-frame
+      # ret, frame = cap.read()
+
+        # do what you want with frame
+        #  and then save to file
+      #  cv2.imwrite('/home/pi/Desktop/Bot/venv/Picture/image.png', frame)
+
+        # When everything done, release the capture
+        #cap.release()
+        #cv2.destroyAllWindows()
+
+    ########################################################################################################
+
+        #imagePath = Path("/home/pi/Desktop/Bot/venv/Picture/image.png")
+
+        #setting variables
+       # url= "http://localhost:1880/ORBface"
+
+       # file ={'image':open(str(imagePath),'rb')}
+
+        #sending a request to node red
+       # r = requests.post(url, files=file)
+      #  print(r.text)
+#
+##############################################################################################################
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+##############################################################################################################
+
+    else:
+
+        cap = cv2.VideoCapture(0)
+
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+
+        # do what you want with frame
+        #  and then save to file
+        cv2.imwrite('/home/pi/Desktop/Bot/venv/Picture/image.png', frame)
+
+        # When everything done, release the capture
+        cap.release()
+        cv2.destroyAllWindows()
+
+    ########################################################################################################
+
+        imagePath = Path("/home/pi/Desktop/Bot/venv/Picture/image.png")
+
+        #setting variables
+        url= "http://localhost:1880/ORBobject"
+
+        file ={'image':open(str(imagePath),'rb')}
+
+        #sending a request to node red
+        r = requests.post(url, files=file)
+        print(r.text)
